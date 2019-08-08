3554e77e3ada96f5771b0b08e5d5186841552ba5
Abhishek <abhishek.madepogu@gmail.com>
2019-08-08 19:58:32 -0400

    [Abhishek]Added Cell

--- app/src/main/java/com/abhishek/c0753379_mad3125_midterm/HomeAdapter.java
+++ app/src/main/java/com/abhishek/c0753379_mad3125_midterm/HomeAdapter.java
@@ -1,5 +1,6 @@
 package com.abhishek.c0753379_mad3125_midterm;
 
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
@@ -7,6 +8,14 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+
+        }
+    }
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
