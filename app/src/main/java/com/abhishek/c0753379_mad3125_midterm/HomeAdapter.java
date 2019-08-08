a553c3ca0ecf594d9712479afd9e9fa70fea8e5e
Abhishek <abhishek.madepogu@gmail.com>
2019-08-08 19:37:21 -0400

    [Abhishek]Added Adapter

---
+++ app/src/main/java/com/abhishek/c0753379_mad3125_midterm/HomeAdapter.java
@@ -0,0 +1,25 @@
+package com.abhishek.c0753379_mad3125_midterm;
+
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
